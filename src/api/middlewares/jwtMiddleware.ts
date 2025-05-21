e4d7e9b56eea3e6cac1402e61196668c500d63a4
douglas.oliveira <douglasoliveira320@gmail.com>
2025-05-21 10:48:31 -0300

    ajusta JWT

--- src/api/middlewares/jwtMiddleware.ts
+++ src/api/middlewares/jwtMiddleware.ts
@@ -1,6 +1,8 @@
 import { Request, Response, NextFunction } from "express";
 import jwt from "jsonwebtoken";
 
+import "dotenv/config";
+
 const SECRET = process.env.JWT_SECRET || "";
 
 export function JWTToken(req: Request, res: Response, next: NextFunction) {
@@ -12,6 +14,8 @@ export function JWTToken(req: Request, res: Response, next: NextFunction) {
 
     try {
       const decoded = jwt.verify(token, SECRET);
+      console.log(decoded, "----", SECRET);
+
       req.query.cpf = decoded;
     } catch (err) {}
   }
